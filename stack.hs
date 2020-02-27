469a64b3888c19581fef24bc65c7cc1c83275fab
monicaholliday <monica.holliday@ba-racing-team.de>
2020-02-26 22:39:47 -0800

    Attempted to implement macros

--- stack.hs
+++ stack.hs
@@ -4,6 +4,9 @@ import Prelude
 import Data.List
 
 type Prog = [Cmd]
+type Stack = [Block]
+type Macro = String
+type Dict = [(String, Prog)]
 
 data Cmd = PushN Int
          | PushB Bool
@@ -23,7 +26,8 @@ data Cmd = PushN Int
          | Lt
          | Concat
          | IfElse Prog Prog
-         | Macro Prog
+         | Define Macro Prog
+         | Call Macro
          | Loop
     deriving (Eq,Show)
 
@@ -34,56 +38,58 @@ data Block
         | P Prog
     deriving (Eq,Show)
 
-type Stack = [Block]
-
 -- I think when we do static typing we won't need the maybe
-cmd :: Cmd -> Stack -> Stack
-cmd (PushN i) s     = I i : s
-cmd (PushB b) s     = B b : s
-cmd (PushS c) s     = S c : s
-cmd (PushP p) s     = P p : s
-cmd Drop      s     = case s of
-                        (a : s')         -> s'
-cmd Dup       s     = case s of 
-                        (a : s')         -> (a : a : s')
-cmd Swap      s     = case s of
-                        (a : b : s')     -> (b : a : s')
-cmd Over      s     = case s of
-                        (a : b : s')     -> (a : b : a : s')
-cmd Rot       s     = case s of
-                        (a : b : c : s') -> (b : c : a : s')
-cmd Add       s     = case s of
-                        (I a : I b : s') -> (I (a+b) : s')
-cmd Sub       s     = case s of
-                        (I a : I b : s') -> (I (a-b) : s')
-cmd Mul       s     = case s of
-                        (I a : I b : s') -> (I (a*b) : s')
-cmd Div       s     = case s of
-                        (I c : I d : s') -> (I (c `div` d) : s')
-cmd Equ       s     = case s of
-                        (I a : I b : s') -> (B (a == b) : s')
-                        (B c : B d : s') -> (B (c == d) : s')
-                        (S e : S f : s') -> (B (e == f) : s')
-cmd Gt        s     = case s of
-                        (I a : I b : s') -> (B (a>b) : s')
-cmd Lt        s     = case s of
-                        (I a : I b : s') -> (B (a<b) : s')
-cmd Concat    s     = case s of
-                        (S a : S b : s') -> (S (a++b) : s')
-cmd (IfElse t e) s  = case s of
-                        (B True : s')    -> prog t s'
-                        (B False : s')   -> prog e s' 
-cmd Loop      s     = case s of
-                        (P p : I a : I b : s') -> loop p s' [I a, I b]
+cmd :: Cmd -> Stack -> Dict -> (Stack, Dict)
+cmd (PushN i) s  d  = ((I i : s), d)
+cmd (PushB b) s  d  = ((B b : s), d)
+cmd (PushS c) s  d  = ((S c : s), d)
+cmd (PushP p) s  d  = ((P p : s), d)
+cmd Drop      s  d  = case s of
+                        (a : s')         -> (s', d)
+cmd Dup       s  d  = case s of 
+                        (a : s')         -> ((a : a : s'), d)
+cmd Swap      s  d  = case s of
+                        (a : b : s')     -> ((b : a : s'), d)
+cmd Over      s  d  = case s of
+                        (a : b : s')     -> ((a : b : a : s'), d)
+cmd Rot       s  d  = case s of
+                        (a : b : c : s') -> ((b : c : a : s'), d)
+cmd Add       s  d  = case s of
+                        (I a : I b : s') -> ((I (a+b) : s'), d)
+cmd Sub       s  d  = case s of
+                        (I a : I b : s') -> ((I (a-b) : s'), d)
+cmd Mul       s  d  = case s of
+                        (I a : I b : s') -> ((I (a*b) : s'), d)
+cmd Div       s  d  = case s of
+                        (I c : I g : s') -> ((I (c `div` g) : s'), d)
+cmd Equ       s  d  = case s of
+                        (I a : I b : s') -> ((B (a == b) : s'), d)
+                        (B c : B g : s') -> ((B (c == g) : s'), d)
+                        (S e : S f : s') -> ((B (e == f) : s'), d)
+cmd Gt        s  d  = case s of
+                        (I a : I b : s') -> ((B (a>b) : s'), d)
+cmd Lt        s  d  = case s of
+                        (I a : I b : s') -> ((B (a<b) : s'), d)
+cmd Concat    s  d  = case s of
+                        (S a : S b : s') -> ((S (a++b) : s'), d)
+cmd (IfElse t e) s d = case s of
+                        (B True : s')    -> prog t s' d
+                        (B False : s')   -> prog e s' d
+cmd Loop      s  d   = case s of
+                        (P p : I a : I b : s') -> ((loop p s' [I a, I b]), d)
+cmd (Define n p) s d = (s, ((n, p) : d))
+cmd (Call n)  s  d  = case lookup n d of
+                        Just p -> prog p s d
+                        Nothing -> (s, d)
 
-loop :: Prog -> Stack -> Stack -> Stack
-loop cmds ds cs = case cs of
-                    (I a : I b : cs') -> if a < b then (loop cmds (prog cmds ds) (I (a+1) : I b : cs') ) else ds
+loop :: Prog -> Stack -> Stack -> Dict -> Stack
+loop cmds ds cs d = case cs of
+                    (I a : I b : cs') -> if a < b then (loop cmds (prog cmds ds d) (I (a+1) : I b : cs') ) else ds
 
-prog :: Prog -> Stack -> Stack
-prog [] s       = s
-prog (c : cs) s    = case cmd c s of
-                       s' -> prog cs s'
+prog :: Prog -> Stack -> Dict -> (Stack, Dict)
+prog [] s d         = (s, d)
+prog (c : cs) s d   = case cmd c s d of
+                       (s', d') -> prog cs s' d'
                      
 
 
